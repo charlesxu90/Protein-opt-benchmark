b9a8dc59e48c6be527f4a1c9e7d7d069bfe04e59
xux <xux@kaust.edu.sa>
2026-06-03 09:42:15 +0300

    rm EvoPlay

--- EvoPlay/run_CR9114.py
+++
@@ -1,1 +0,0 @@
-/home/xux/Desktop/AlphaVariant/Benchmark/scripts/EvoPlay/run_CR9114.py
--- EvoPlay/run_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run EvoPlay on GB1 dataset
-python run_GB1.py --seed_file ../rand_seeds.txt --num_seeds 50 --use_gpu
-
-# Run with GPU support
-# python run_GB1.py --seed_file src/rndseed.txt --num_seeds 50
